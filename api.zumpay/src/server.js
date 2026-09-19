1ce65812f2c0455ec0016ae355b6a8abc377cafd
polwiker1 <cervezawicca@hotmail.com.ar>
2026-09-19 17:15:40 -0300

    chore: remove migrated API services

--- api.zumpay/src/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-const app = require("./app");
-const { env } = require("./config/env");
-
-app.listen(env.port, () => {
-  console.log(`Zumpay Arc API listening on port ${env.port}`);
-});
